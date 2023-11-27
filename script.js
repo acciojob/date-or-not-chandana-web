17dd79c36259b786f31906ab5ae70d6408b5d15f
chandana-web <143087553+chandana-web@users.noreply.github.com>
2023-11-27 15:49:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 var isDate = function (input) {
-  //   write your code here
-};
+   if (Object.prototype.toString.call(input) === '[object Date]') {
+    // Check if it's a valid Date object
+    return !isNaN(input.getTime());
+  }
 
-// Do not change the code below.
+  // Check if the input is a string
+  if (typeof input === 'string') {
+    // Try to parse the string into a Date object
+    const parsedDate = new Date(input);
+
+    // Check if the parsedDate is a valid Date object
+    return !isNaN(parsedDate.getTime());
+  }
+
+  
+  return false;
+};// Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
